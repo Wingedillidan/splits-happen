319e2f333e8aeca8511e1b24c54ff3d17f89e67b
Collin McLean <collinm@rainn.org>
2017-06-07 15:51:52 -0400

    Test mode activate

--- SplitsHappen.java
+++ SplitsHappen.java
@@ -8,16 +8,22 @@ import main.java.tests.GameTest;
 public class SplitsHappen {
 
     public static void main(String[] args) {
-        Game game;
-        String[] test = new String[]{"XXXXXXXXXXXX", "9-9-9-9-9-9-9-9-9-9-", "5/5/5/5/5/5/5/5/5/5/5", "X7/9-X-88/-6XXX81"};
+        if (args[0] == "-debug" || args[0] == "-d") {
+            debug();
+        } else {
+            Game game;
+            String[] test = new String[]{"XXXXXXXXXXXX", "9-9-9-9-9-9-9-9-9-9-", "5/5/5/5/5/5/5/5/5/5/5", "X7/9-X-88/-6XXX81"};
 
-        for (String s: test) {
-            game = new Game(s);
-            game.calcGame();
-            System.out.println(game.toString());
-            System.out.println("Total: " + game.getTotal());
+            for (String s : test) {
+                game = new Game(s);
+                game.calcGame();
+                System.out.println(game.toString());
+                System.out.println("Total: " + game.getTotal());
+            }
         }
+    }
 
+    private static void debug() {
         GameTest.testAll();
     }
 
