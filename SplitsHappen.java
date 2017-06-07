33d972269cff525e6b205d388c2bebd4e6973d73
Collin McLean <collinm@rainn.org>
2017-06-06 21:04:42 -0400

    oops

--- SplitsHappen.java
+++ SplitsHappen.java
@@ -5,7 +5,7 @@ package main.java;
  */
 public class SplitsHappen {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Game game;
 
         String testOne = "XXXXXXXXXXXX";
