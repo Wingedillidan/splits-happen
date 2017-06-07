a3b37cca16140c4e86efdda6d2be50ebf2e728ad
Collin McLean <collinm@rainn.org>
2017-06-06 20:44:16 -0400

    Whole game string parsing

--- Game.java
+++ Game.java
@@ -5,14 +5,40 @@ package main.java;
  */
 public class Game {
 
-    public Game(String raw) {
+    private Frame[] frames;
+    private int total;
 
+    public Game(String raw) {
+        parse(raw);
     }
 
-    private static void parse(String input) {
-        for (char c: input) {
+    private void parse(String input) {
+        Frame[] result = new Frame[10]; // Out of scope task #3 assumption of 10 frames per string
+        String stringFrame = "";
+        int currentFrame = 0;
 
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'X') {
+                result[currentFrame] = new Frame("X");
+                currentFrame++;
+            } else {
+                stringFrame += input.charAt(i);
+            }
+
+            // Check for 10th frame
+            if (i + 1 == input.length() - 1) {
+                stringFrame += input.charAt(i + 1);
+                i++;
+            }
+            // Make the frame
+            if (stringFrame.length() >= 2) {
+                result[currentFrame] = new Frame(stringFrame);
+                currentFrame++;
+                stringFrame = "";
+            }
         }
+
+        this.frames = result;
     }
 
 }
